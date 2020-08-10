b5c5c61a82a660b53a06cd0f9825ac475e6b830b
Alex Wishinkiy <basstraper@gmail.com>
2020-08-11 02:24:07 +0300

    Add files via upload

---
+++ js/paralax.js
@@ -0,0 +1,39 @@
+  var image = document.getElementsByClassName('ione');
+  new simpleParallax(image, {
+      scale: 1.8
+  });
+
+  var image = document.getElementsByClassName('itwo');
+  new simpleParallax(image, {
+      scale: 1.6,
+      orientation: 'left'
+  });
+
+  var image = document.getElementsByClassName('ithree');
+  new simpleParallax(image, {
+      scale: 1.6,
+      orientation: 'right'
+  });
+
+  var image = document.getElementsByClassName('ifour');
+  new simpleParallax(image, {
+      scale: 1.6,
+  });
+
+  var image = document.getElementsByClassName('ifive');
+  new simpleParallax(image, {
+      scale: 1.6,
+      orientation: 'right'
+  });
+
+  var image = document.getElementsByClassName('isix');
+  new simpleParallax(image, {
+      scale: 1.6,
+      orientation: 'left'
+  });
+
+  $(document).ready(function() {
+      $(".menu-icon").on("click", function() {
+          $("nav ul").toggleClass("showing");
+      });
+  });
